ae98c4d7acfd800ae62bdeea851cd87538b7bb0c
mauricius <maurizio.bonani@gmail.com>
2020-10-11 15:43:18 +0200

    Add getTextdomain function

--- helpers.php
+++ helpers.php
@@ -15,3 +15,19 @@ if( ! function_exists('_n'))
         return ProcessWire\_n($textSingular, $textPlural, $count, $textdomain);
     }
 }
+
+
+if (! function_exists('getTextdomain'))
+{
+    function getTextdomain(string $file)
+    {
+        $config = ProcessWire\wire('config');
+
+        $textdomain = str_replace($config->paths->root, '', $file);
+        $textdomain = str_replace('/', '--', $textdomain);
+        $textdomain = str_replace('.', '-', $textdomain);
+
+        return $textdomain;
+    }
+}
+}
